47cc73d496844724b0b72f825c38a06de5f9c520
Maria Sophia M. Solo <solomariasophia@gmail.com>
2024-01-27 18:23:22 +0800

    trial done-working

--- pynative_string.py
+++ pynative_string.py
@@ -9,15 +9,15 @@ def remove_chars (name, n, from_end=False):
     else:
         x = name[n:]
 # return
-        return
+    return x
 
 # ask user to input string and the number of characters to remove
 user_input = input("Enter your name: ")
 n = int(input("Enter how many letters you want to remove from your name: "))
 
 # ask user whether to remove charcaters from the end or beginning
-from_end_input = input("Do you want it to remove from end or beginning? (y/n:) ").lower()
-from_end = from_end_input == 'y'
+from_end_input = input("Do you want it to remove from end? (yes/no):").lower()
+from_end = from_end_input == 'yes'
 
 # call the function with the user input
 result = remove_chars (user_input, n, from_end)
